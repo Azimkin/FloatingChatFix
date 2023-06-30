2708fc500796d693a743c9a07ea0bdfaadc09db4
Azimkin <112006710+Azimkin@users.noreply.github.com>
2023-06-30 21:25:41 +0200

    Plugin v1 (official v4, but v1 in this repo)

---
+++ src/main/java/top/floatingchatfix/FloatingChatFix.java
@@ -0,0 +1,17 @@
+package top.floatingchatfix;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class FloatingChatFix extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
